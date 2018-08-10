ffae80437a3b308d2fe2c152a05a3ff270934358
Tomas Kral <tomas.kral@gmail.com>
2018-08-10 11:55:52 +0200

    v0.0.9

--- Formula/odo.rb
+++ Formula/odo.rb
@@ -1,8 +1,8 @@
 class Odo < Formula
   desc "OpenShift Command-line for Developers"
   homepage "https://github.com/redhat-developer/odo"
-  url "https://github.com/redhat-developer/odo/archive/v0.0.8.tar.gz"
-  sha256 "38c8df7b30026db3b87f92008596bb7873415e98c61abf739491b8739cc148aa"
+  url "https://github.com/redhat-developer/odo/archive/v0.0.9.tar.gz"
+  sha256 "43228304417d19e073de5be35899d5b12d33285437c55fd96dd6cfd52a8389df"
 
   head do
     url "https://github.com/redhat-developer/odo.git"
