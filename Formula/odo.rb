55c2ca4652a0a45018a548c78297fc7c518dd1b2
Tomas Kral <tkral@redhat.com>
2019-10-18 14:21:35 +0200

    update to v1.0.0

--- Formula/odo.rb
+++ Formula/odo.rb
@@ -3,19 +3,19 @@ class Odo < Formula
   homepage "https://github.com/openshift/odo"
 
   url "https://github.com/openshift/odo.git",
-      :tag      => "v1.0.0-beta7",
-      :revision => "785a922d079e9f94f09f2e57666eb388c8c34874",
-      :shallow  => false
+      tag: "v1.0.0",
+      revision: "4e3175e6f1aa0b774a15cde1a4069619a43f4e7a",
+      shallow: false
 
   head "https://github.com/openshift/odo.git",
-      :shallow  => false
+       shallow: false
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/openshift/odo").install buildpath.children
-    cd buildpath/"src/github.com/openshift/odo" do
+    (buildpath / "src/github.com/openshift/odo").install buildpath.children
+    cd buildpath / "src/github.com/openshift/odo" do
       system "make", "bin"
       bin.install "odo"
     end
@@ -23,9 +23,12 @@ class Odo < Formula
 
   test do
     # all other odo commands require running OpenShift cluster
-    shell_output("#{bin}/odo version --client || true")
+    version_output = shell_output("#{bin}/odo version 2>&1")
+
+    tag = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:tag]
+    short_rev = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision].slice(0, 8)
+    assert_match tag, version_output
+    assert_match short_rev, version_output
+
   end
 end
-
-
-
