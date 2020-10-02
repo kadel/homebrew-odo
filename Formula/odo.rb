102f8d3226705ca3205c6b3638d9f1598d0c74f4
Tomas Kral <tkral@redhat.com>
2020-10-02 15:47:08 +0200

    v2.0.0

--- Formula/odo.rb
+++ Formula/odo.rb
@@ -1,11 +1,11 @@
 class Odo < Formula
-  desc "OpenShift Command-line for Developers"
+  desc "Developer-focused CLI for Kubernetes and OpenShift"
   homepage "https://github.com/openshift/odo"
-  
+
   url "https://github.com/openshift/odo.git",
-  :tag      => "v1.2.5",
-  :revision => "0aeec48e4db427385128fd98c0c2ec24e756387a"
-  
+  :tag      => "v2.0.0",
+  :revision => "6fbb9d9bfa0a737a74b71735326f1d82abf773f9"
+
   head "https://github.com/openshift/odo.git"
 
   depends_on "go" => :build
@@ -20,12 +20,19 @@ class Odo < Formula
   end
 
   test do
-    # all other odo commands require running OpenShift cluster
+    # test version
     version_output = shell_output("#{bin}/odo version 2>&1")
-
     tag = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:tag]
     short_rev = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision].slice(0, 8)
-    assert_match tag, version_output
-    assert_match short_rev, version_output
+    assert_match "odo #{tag} (#{short_rev})", "#{version_output}"
+    #assert_match short_rev, version_output
+
+    # try set preference
+    ENV["GLOBALODOCONFIG"] = "#{testpath}/preference.yaml"
+    shell_output("#{bin}/odo preference set UpdateNotification true")
+    assert_predicate testpath/"preference.yaml", :exist?
+
+    # almost all other odo commands require connection to OpenShift cluster 
   end
 end
+
